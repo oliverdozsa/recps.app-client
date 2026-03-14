f80c1d55f44b663aed7ded5137e564782ef31bf2
Oliver Dozsa <dozsa.oliver@gmail.com>
2026-03-14 20:41:55 +0100

    Angular OIDC.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {OAuthStorage, provideOAuthClient} from 'angular-oauth2-oidc';
+import {environment} from '../environments/environment';
+
+export function storageFactory(): OAuthStorage {
+  return localStorage
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideOAuthClient({resourceServer: {allowedUrls: [environment.apiUrl, "localhost"], sendAccessToken: true}}),
+    {provide: OAuthStorage, useFactory: storageFactory},
+  ]
 };
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,55 @@
+import {inject, Injectable} from '@angular/core';
+import {OAuthService} from 'angular-oauth2-oidc';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private oauthService = inject(OAuthService);
+  private router = inject(Router);
+
+  get isLoggedIn() {
+    return this.oauthService.hasValidIdToken();
+  }
+
+  constructor() {
+    this.oauthService.setStorage(localStorage);
+
+    this.oauthService.configure({
+      issuer: "https://accounts.google.com",
+      strictDiscoveryDocumentValidation: false,
+      clientId: "39099012072-mtapaq50ir89bif3uipttsuhk550te1v.apps.googleusercontent.com",
+      redirectUri: window.location.origin + "/home",
+      scope: "openid profile email"
+    });
+
+    this.oauthService.setupAutomaticSilentRefresh();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin()
+      .then(() => this.onTryLoginComplete());
+  }
+
+  login() {
+    this.oauthService.initLoginFlow(this.router.url);
+  }
+
+  logout() {
+    this.oauthService.revokeTokenAndLogout();
+    this.oauthService.logOut();
+  }
+
+  getProfile() {
+    return this.oauthService.getIdentityClaims();
+  }
+
+  private onTryLoginComplete() {
+    let stateUrl = this.oauthService.state!;
+    if (stateUrl) {
+      if (!stateUrl.startsWith('/')) {
+        stateUrl = decodeURIComponent(stateUrl);
+      }
+
+      setTimeout(() =>this.router.navigateByUrl(stateUrl));
+    }
+  }
+}
