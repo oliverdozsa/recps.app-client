6a5af711997456cf76a18d2ba8ae449bf42ca151
Oliver Dozsa <dozsa.oliver@gmail.com>
2026-03-15 19:04:08 +0100

    Login button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {AuthService} from './services/auth.service';
+import {NgIf, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'recps.app-client';
+
+  authService = inject(AuthService);
 
   constructor(private router: Router) {
   }
@@ -25,5 +29,11 @@ export class AppComponent implements OnInit {
     });
   }
 
+  get picture() {
+    return this.authService.getProfile()["picture"];
+  }
 
+  get name() {
+    return this.authService.getProfile()["name"];
+  }
 }
