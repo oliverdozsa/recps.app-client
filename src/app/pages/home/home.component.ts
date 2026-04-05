28f3ff1d2b4d7efedbe3639aa990f6bd6d2235b6
Oliver Dozsa <dozsa.oliver@gmail.com>
2026-04-05 09:43:39 +0200

    Querying languages.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import {Component, inject, OnInit, signal} from '@angular/core';
-import {TagsInputComponent} from '../../components/tags-input/tags-input.component';
 import {
   RecipeSearchResultDisplayComponent
 } from '../../components/recipe-search-result-display/recipe-search-result-display.component';
@@ -10,7 +9,9 @@ import {
   RecipeMainSearchParamsComponent
 } from '../../components/recipe-main-search-params/recipe-main-search-params.component';
 import {RecipeService} from '../../services/recipe.service';
+import {LanguageService} from '../../services/language.service';
 import {RecipeSearchResponse} from '../../services/responses';
+import {forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -22,20 +23,30 @@ import {RecipeSearchResponse} from '../../services/responses';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   private recipeService = inject(RecipeService);
+  private languageService = inject(LanguageService);
 
   filterByName = signal('');
   recipes = signal<RecipeSearchResponse[]>([]);
+  loading = signal(false);
 
   search(): void {
-    this.recipeService.search({ filterByName: this.filterByName() }).subscribe(response => {
+    this.recipeService.search({filterByName: this.filterByName()}).subscribe(response => {
       this.recipes.set(response.items ?? []);
     });
   }
 
   ngOnInit(): void {
-    this.search();
+    this.loading.set(true);
+    forkJoin([
+      this.recipeService.search({filterByName: this.filterByName()}),
+      this.languageService.getAll()
+    ]).subscribe({
+      next: ([recipesResponse]) => {
+        this.recipes.set(recipesResponse.items ?? []);
+      },
+      complete: () => this.loading.set(false)
+    });
   }
-
 }
--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,6 +1,6 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, tap} from 'rxjs';
 import {LanguageResponse} from './responses';
 import {environment} from '../../environments/environment';
 
@@ -12,7 +12,11 @@ export class LanguageService {
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
 
+  languages = signal<LanguageResponse[]>([]);
+
   getAll(): Observable<LanguageResponse[]> {
-    return this.http.get<LanguageResponse[]>(`${this.baseUrl}/languages`);
+    return this.http.get<LanguageResponse[]>(`${this.baseUrl}/languages`).pipe(
+      tap(langs => this.languages.set(langs))
+    );
   }
 }
